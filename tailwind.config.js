1b41072093dc3248231401f7f90236fa895ee7ed
unknown <susilatropika8@gmail.com>
2025-12-06 00:57:38 +0700

    fix: add tailwindcss to dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,6 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    // Tambahkan semua path yang mungkin
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
